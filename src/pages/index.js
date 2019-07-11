3d8650d06c12206b5060a9101c3afebd9925fac4
patrick <patrick.morris@nytimes.com>
2019-07-11 19:11:56 -0400

    small updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to our wedding website</h1>
+    <p>We're looking forward to you joining us from our special day.</p>
+    <p>September 14, 2019</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
