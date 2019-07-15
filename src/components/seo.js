ff3e6c6cc4bc7f9c0a02f9fced2b83505ae1be0f
patrick <patrick.morris@nytimes.com>
2019-07-14 23:06:48 -0400

    cleanup

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,88 +1,144 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+const config = {
+  siteTitle: 'Fabian Schultz',
+  author: 'Fabian Schultz',
+  siteDescription: `Hello, I'm Fabian Schultz — a product designer and developer based in Potsdam, Germany.`,
+  siteUrl: 'https://fabianschultz.com',
+  userTwitter: 'fschultz_',
+};
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
+class SEO extends Component {
+  render() {
+    const { postNode, postPath, postSEO, pageSEO } = this.props;
+    const { siteTitle, siteUrl, siteDescription, userTwitter, author } = config;
+    let title;
+    let description;
+    let image;
+    let postURL;
 
-  const metaDescription = description || site.siteMetadata.description
+    if (postSEO || pageSEO) {
+      const postMeta = postNode;
+      title = `${siteTitle} | ${postMeta.subtitle}`;
+      description = postMeta.excerpt ? postMeta.excerpt : postNode.title;
+      image = postMeta.cover.childImageSharp.sizes.src;
+      postURL = siteUrl + postPath;
+    } else {
+      title = siteTitle;
+      description = siteDescription;
+      image = '/covers/hero-bw.jpg';
+    }
+    image = siteUrl + image;
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
+    const schemaOrgJSONLD = [
+      {
+        '@context': 'http://schema.org',
+        '@type': 'WebSite',
+        url: siteUrl,
+        name: title,
+        alternateName: '',
+      },
+    ];
+    if (postSEO) {
+      schemaOrgJSONLD.push({
+        '@context': 'http://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: [
+          {
+            '@type': 'ListItem',
+            position: 1,
+            item: {
+              '@id': `${siteUrl}/work`,
+              name: 'Work',
+            },
+          },
+          {
+            '@type': 'ListItem',
+            position: 2,
+            item: {
+              '@id': postURL,
+              name: title,
+              image,
+            },
+          },
+        ],
+      });
+
+      schemaOrgJSONLD.push({
+        '@context': 'http://schema.org',
+        '@type': 'BlogPosting',
+        url: postURL,
+        name: title,
+        author: {
+          '@type': 'Person',
+          name: author,
         },
-        {
-          property: `og:title`,
-          content: title,
+        publisher: {
+          '@type': 'Organization',
+          name: siteTitle,
+          logo: {
+            '@type': 'ImageObject',
+            url: 'https://fabianschultz.com/publisher.jpg',
+          },
         },
-        {
-          property: `og:description`,
-          content: metaDescription,
+        datePublished: postNode.date,
+        alternateName: '',
+        headline: postNode.subtitle,
+        image: {
+          '@type': 'ImageObject',
+          url: image,
         },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
-  )
+        description,
+      });
+    }
+
+    if (pageSEO) {
+      schemaOrgJSONLD.push({
+        '@context': 'http://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: [
+          {
+            '@type': 'ListItem',
+            position: 1,
+            item: {
+              '@id': postURL,
+              name: title,
+              image,
+            },
+          },
+        ],
+      });
+    }
+
+    return (
+      <Helmet>
+        {/* General tags */}
+        <meta name="description" content={description} />
+        <meta name="image" content={image} />
+        <link rel="canonical" href={postSEO ? postURL : siteUrl} />
+
+        {/* Schema.org tags */}
+        <script type="application/ld+json">
+          {JSON.stringify(schemaOrgJSONLD)}
+        </script>
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={postSEO ? postURL : siteUrl} />
+        {postSEO ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={userTwitter ? userTwitter : ''} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
+    );
+  }
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+export default SEO;
