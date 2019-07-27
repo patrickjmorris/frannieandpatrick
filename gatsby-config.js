838c1d060e42843b7994a143620d1eacdf4b370c
patrick <patrick.morris@nytimes.com>
2019-07-26 20:55:29 -0400

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-144546338-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'frannieandpatrick.com',
+      },
+    },
   ],
 };
